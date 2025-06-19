53df3b4586d4ccc96168c3ea653b8796351d9218
chiheb <chihebabiza9@gmail.com>
2025-06-19 13:11:49 +0100

    queue array done

--- My-queue-array/My-queue-array.cpp
+++ My-queue-array/My-queue-array.cpp
@@ -1,20 +1,69 @@
-// My-queue-array.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "clsMyQueueArr.h"
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    clsMyQueueArr <int> MyQueue;
+
+    MyQueue.push(10);
+    MyQueue.push(20);
+    MyQueue.push(30);
+    MyQueue.push(40);
+    MyQueue.push(50);
+
+
+    cout << "\nQueue: \n";
+    MyQueue.Print();
+
+    cout << "\nQueue Size: " << MyQueue.Size();
+    cout << "\nQueue Front: " << MyQueue.front();
+    cout << "\nQueue Back: " << MyQueue.back();
+
+    MyQueue.pop();
+
+    cout << "\n\nQueue after pop() : \n";
+    MyQueue.Print();
+
+    //Extension #1
+    cout << "\n\n Item(2) : " << MyQueue.GetItem(2);
+
+    //Extension #2
+    MyQueue.Reverse();
+    cout << "\n\nQueue after reverse() : \n";
+    MyQueue.Print();
+
+    //Extension #3
+    MyQueue.UpdateItem(2, 600);
+    cout << "\n\nQueue after updating Item(2) to 600 : \n";
+    MyQueue.Print();
+
+    //Extension #4
+    MyQueue.InsertAfter(2, 800);
+    cout << "\n\nQueue after Inserting 800 after Item(2) : \n";
+    MyQueue.Print();
+
+
+    //Extension #5
+    MyQueue.InsertAtFront(1000);
+    cout << "\n\nQueue after Inserting 1000 at front: \n";
+    MyQueue.Print();
+
+    //Extension #6
+    MyQueue.InsertAtBack(2000);
+    cout << "\n\nQueue after Inserting 2000 at back: \n";
+    MyQueue.Print();
+
+    //Extension #7
+    MyQueue.Clear();
+    cout << "\n\nQueue after Clear(): \n";
+    MyQueue.Print();
+
+
+
+
+    system("pause>0");
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
